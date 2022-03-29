501b1a8ae58666acc543469f53d61363785e9aff
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-03-29 12:55:29 +0100

    Clean up initial project

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,4 @@
 /**
  * Main Class
  */
-export class Main {
-  public static COMMANDS = 'Hello World';
-
-  public param: string;
-
-  constructor(param: string) {
-    this.param = param;
-  }
-
-  public getParam(): string {
-    return this.param;
-  }
-}
+export class Main {}
