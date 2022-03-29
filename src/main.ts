8c35f1ffae0641942760c23499bb61684dabbfc8
Daniel Álvarez Medina <alu0101216126@ull.edu.es>
2022-03-29 13:42:07 +0100

    Test success: Should return the same letter adding 'ay' at the end of each letter

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@
  */
 export class Main {
   public pigIt(phrase: string): string {
-    return phrase + 'ay';
+    let phraseSplitted = phrase.split(' ');
+    phraseSplitted = phraseSplitted.map(word => {
+      return word + 'ay';
+    });
+
+    const result = phraseSplitted.join(' ');
+    return result;
   }
 }
